a14c3a8f79ce667c480bcba8b97b25d7d556e222
Asil <90615063+Asroyx@users.noreply.github.com>
2022-10-19 16:41:21 +0300

    second commit
    comments updated

--- LFSR.py
+++ LFSR.py
@@ -2,4 +2,5 @@ state = 0b10011001
 for i in range(5):
      print("{:08b}".format(state))
      newbit = (state ^ (state >> 1)) & 1 #[newbite] taking last index of [state]
-     state = (state >> 1) | (newbit << 7) #[newbit] was 1 after (newbit<< 7) having new value newbit = 10000000, (state = 01001100 | 10000000) = 11001100
+     state = (state >> 1) | (newbit << 7) #[newbit] was 1 after (newbit<< 7) having new value newbit = 10000000
+                                          # (state = 01001100 | 10000000) = 11001100
